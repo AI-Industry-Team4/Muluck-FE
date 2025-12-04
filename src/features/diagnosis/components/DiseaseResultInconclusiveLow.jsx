8c21fa730fba7f670f2f3c852b9a77fc9bbe2fc4
siiion <siwonjeon234@gmail.com>
2025-12-04 11:29:02 +0900

    Feat/#5/inconclusive low 컴포넌트 구현

--- src/features/diagnosis/components/DiseaseResultInconclusiveLow.jsx
+++ src/features/diagnosis/components/DiseaseResultInconclusiveLow.jsx
@@ -1,5 +1,26 @@
-import { Body20 } from '@/shared/typography'
+import { Body20, Head25 } from '@/shared/typography'
+import Button from '@/shared/components/Button'
 
 export default function DiseaseResultInconclusiveLow() {
-  return <Body20>확률 20% 미만</Body20>
+  return (
+    <div className='flex flex-col justify-between'>
+      {/* 메인 영역 */}
+      <div className='flex flex-col justify-center items-center mt-[44px] gap-[27px] py-[27px] rounded-[10px] border border-[2px] border-percent'>
+        <Head25 className='text-gray-300'>병충해를 판별할 수 없어요 😢</Head25>
+
+        <Body20 className='text-center whitespace-pre-line'>
+          {`병충해가 의심되는 잎을\n확대해서 촬영해 주세요!`}
+        </Body20>
+      </div>
+
+      {/* 저장하기 버튼 */}
+      <Button
+        label='사진 다시 촬영하기'
+        size='large'
+        variant='primary'
+        className='self-center mt-[42px]'
+        onClick={() => console.log('사진 재촬영 클릭')}
+      />
+    </div>
+  )
 }
