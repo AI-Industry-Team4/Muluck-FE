f8309957ec9afb616c8c5a59a01c472e26f3b141
siiion <siwonjeon234@gmail.com>
2025-12-04 11:26:56 +0900

    Feat/#5/inconclusive mid 컴포넌트 구현

--- src/features/diagnosis/components/DiseaseResultInconclusiveMid.jsx
+++ src/features/diagnosis/components/DiseaseResultInconclusiveMid.jsx
@@ -1,5 +1,43 @@
-import { Body20 } from '@/shared/typography'
+import { Body20, Head25 } from '@/shared/typography'
+import Button from '@/shared/components/Button'
 
 export default function DiseaseResultInconclusiveMid() {
-  return <Body20>확률 20% 이상 30% 미만</Body20>
+  // TODO: 실제 데이터로 교체
+  const diseases = ['질병1', '질병2']
+  const confidences = [0, 0]
+
+  return (
+    <div className='flex flex-col justify-between'>
+      {/* 메인 영역 */}
+      <div className='flex flex-col justify-center items-center mt-[44px] gap-[27px] py-[27px] rounded-[10px] border border-[2px] border-percent'>
+        <Head25 className='text-gray-300'>병충해를 판별할 수 없어요 😢</Head25>
+
+        {/* 질병 후보 2개 */}
+        <div>
+          {diseases.map((disease, idx) => {
+            const rank = idx + 1
+
+            return (
+              <div key={idx} className='text-center text-xl leading-[1.56rem] text-gray-100'>
+                후보 {rank}. {disease} (예측 확률: {confidences[idx]}%)
+              </div>
+            )
+          })}
+        </div>
+
+        <Body20 className='text-center whitespace-pre-line'>
+          {`정확한 병충해 판별을 위해\n사진을 다시 촬영해 주세요!`}
+        </Body20>
+      </div>
+
+      {/* 저장하기 버튼 */}
+      <Button
+        label='사진 다시 촬영하기'
+        size='large'
+        variant='primary'
+        className='self-center mt-[42px]'
+        onClick={() => console.log('사진 재촬영 클릭')}
+      />
+    </div>
+  )
 }
