7398ab4303131a997b682cf58ba7fd38698f18bf
siiion <siwonjeon234@gmail.com>
2025-12-07 16:37:52 +0900

    Setting/#17/테스트용 UUID 상수화

--- src/shared/config/env.js
+++ src/shared/config/env.js
@@ -10,6 +10,20 @@ const env = {
   apiBaseUrl: apiBaseUrl,
   // @ts-ignore
   env: process.env.REACT_APP_ENV || 'dev',
+
+  // 테스트/시연용 IDs
+  userId:
+    // @ts-ignore
+    process.env.REACT_APP_USER_ID,
+  potatoFolderId:
+    // @ts-ignore
+    process.env.REACT_APP_FOLDER_ID_POTATO,
+  healthyPotatoDiagnosisId:
+    // @ts-ignore
+    process.env.REACT_APP_DIAGNOSIS_ID_HEALTHY_POTATO,
+  muluckFolderId:
+    // @ts-ignore
+    process.env.REACT_APP_FOLDER_ID_MULUCK,
 }
 
 export default env
