922f2bc206b51e63bd6ec73e244e7cdaf8d877af
seeun <tpdms3139@naver.com>
2025-12-09 01:42:46 +0900

    Feat/#31/진단명 컴포넌트 연동

---
+++ src/api/folderDetail.js
@@ -0,0 +1,18 @@
+import env from '@/shared/config/env'
+import client from './client'
+import { handleApiError } from '@/shared/utils/handleApiError'
+
+// 작물 진단 기록 리스트 조회 API
+export async function getFoldersDetail({ folderId }) {
+  try {
+    const res = await client.get(`/folders/${folderId}/diagnoses`, {
+      headers: {
+        'User-Id': env.userId,
+      },
+    })
+
+    return res.data.data
+  } catch (error) {
+    throw handleApiError(error)
+  }
+}
