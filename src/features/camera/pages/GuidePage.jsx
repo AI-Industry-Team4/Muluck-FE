37a826a97a5d7c1f287f73b0992b153f0c2b041a
seeun <tpdms3139@naver.com>
2025-12-09 14:05:36 +0900

    Design/#34/사진 비율 조정

--- src/features/camera/pages/GuidePage.jsx
+++ src/features/camera/pages/GuidePage.jsx
@@ -2,8 +2,8 @@ import { useNavigate } from 'react-router-dom'
 import { Body20 } from '@/shared/typography'
 import plant from '@/assets/icons/guide_plant.png'
 import guideline from '@/assets/icons/guideline.png'
-import photo from '@/assets/icons/photo.png'
 import Button from '@/shared/components/Button'
+import PhotoUploadButton from '../components/PhotoUploadButton'
 
 export default function GuidePage() {
   const navigate = useNavigate()
@@ -15,10 +15,10 @@ export default function GuidePage() {
       </button>
 
       {/* 콘텐츠 영역 */}
-      <div className='relative h-[560px]'>
-        <img src={plant} alt='guide_plant' className='absolute' />
-        <img src={guideline} alt='guideline' className='absolute z-10 mt-[123px] ml-[23px]' />
-        <div className='w-50 h-25 rounded-[10px] mt-[390px] ml-[175px] absolute z-10 flex flex-col justify-center text-center bg-black/40 gap-2 border border-white'>
+      <div className='relative h-[75vh]'>
+        <img src={plant} alt='guide_plant' className='absolute w-full' />
+        <img src={guideline} alt='guideline' className='absolute z-10 mt-[35%] left-[4%]' />
+        <div className='w-50 h-25 rounded-[10px] mt-[100%] left-[45%] absolute z-10 flex flex-col justify-center text-center bg-black/40 gap-2 border border-white'>
           <Body20 className='text-white'>병충해가 의심되는 잎을</Body20>
           <div className='flex justify-center gap-1'>
             <Body20 className='text-red'>확대해서</Body20>
@@ -28,9 +28,12 @@ export default function GuidePage() {
       </div>
 
       {/* 하단 버튼 */}
-      <div className='flex mt-[63px] ml-[121px]'>
+      <div className='relative w-full flex justify-center z-10'>
         <Button size='small' label='촬영하기' onClick={() => navigate('/camera')} />
-        <img src={photo} alt='photo' className='ml-[39px] cursor-pointer' />
+        <PhotoUploadButton
+          className='absolute left-1/2 translate-x-[100px] cursor-pointer'
+          nextPage='/camera/preview'
+        />
       </div>
     </div>
   )
